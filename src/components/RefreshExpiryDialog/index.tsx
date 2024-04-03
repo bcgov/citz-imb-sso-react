aaeb3fe04b69009d66162373816fd7494760fd7c
BradyMitch <braden.jr.mitch@gmail.com>
2024-04-03 14:48:25 -0700

    Updated refresh dialog styles

--- src/components/RefreshExpiryDialog/index.tsx
+++ src/components/RefreshExpiryDialog/index.tsx
@@ -1,11 +1,28 @@
 import './styles.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { RefreshExpiryDialogProps } from '../../types';
 import { useSSO } from '../../state/useSSO';
 
 export const RefreshExpiryDialog = (props: RefreshExpiryDialogProps) => {
   const { isVisible, loginProps } = props;
   const { login } = useSSO();
+
+  const [message, setMessage] = useState('Your login session is about to expire.');
+  const [buttonText, setButtonText] = useState('STAY LOGGED IN');
+
+  useEffect(() => {
+    if (isVisible)
+      setTimeout(() => {
+        // Change 15 seconds after dialog becomes visible.
+        setMessage('Your login session has expired.');
+        setButtonText('LOG IN AGAIN');
+      }, 15000);
+    else {
+      // Change when dialog becomes invisible.
+      setMessage('Your login session is about to expire.');
+      setButtonText('STAY LOGGED IN');
+    }
+  }, [isVisible]);
 
   if (!isVisible) return null;
 
@@ -14,9 +31,9 @@ export const RefreshExpiryDialog = (props: RefreshExpiryDialogProps) => {
       <div className="ssor_dialog-overlay" />
       <dialog className="ssor_dialog" open={isVisible}>
         <div className="ssor_dialog-content">
-          <p className="ssor_dialog-message">Your login session has expired.</p>
+          <p className="ssor_dialog-message">{message}</p>
           <button className="ssor_button" onClick={() => login(loginProps)}>
-            Log in again
+            {buttonText}
           </button>
         </div>
       </dialog>
