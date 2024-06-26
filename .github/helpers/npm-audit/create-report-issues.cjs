6d41c995e2c663a65fcb38496e6800e7d8699241
Brady Mitchell <braden.jr.mitch@gmail.com>
2024-06-26 09:09:03 -0700

    Update create-report-issues.cjs

--- .github/helpers/npm-audit/create-report-issues.cjs
+++ .github/helpers/npm-audit/create-report-issues.cjs
@@ -1,5 +1,5 @@
 const path = require('path');
-const createAndCloseExistingIssue = require('../../github-api/create-and-close-existing-issue.cjs');
+const createAndCloseExistingIssue = require('../github-api/create-and-close-existing-issue.cjs');
 const outputText = require(path.resolve(__dirname, `../../../outputText.json`));
 
 /**
